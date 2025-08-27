7cd38ebbb1ee3df87ed280daa86ef9a6fd3e23b0
venomous-maker <morganokumu8+1@gmail.com>
2025-08-27 14:48:18 +0300

    Refactored by migrating code

--- src/add_user.sh
+++ src/add_user.sh
@@ -24,25 +24,3 @@ chown "$USERNAME:$USERNAME" "/home/$USERNAME/workspace"
 
 echo "User $USERNAME created successfully with password: $PASSWORD"
 echo "User can SSH to this container and has sudo privileges"
-
----
-
-#!/bin/bash
-# startup.sh - Container startup script
-
-# Start SSH service
-service ssh start
-
-# Check if USERS environment variable is set and create users
-if [ ! -z "$USERS" ]; then
-    IFS=',' read -ra USER_ARRAY <<< "$USERS"
-    for user_info in "${USER_ARRAY[@]}"; do
-        IFS=':' read -ra USER_PASS <<< "$user_info"
-        username=${USER_PASS[0]}
-        password=${USER_PASS[1]:-"defaultpass"}
-        /usr/local/bin/add_user.sh "$username" "$password"
-    done
-fi
-
-# Keep container running
-tail -f /dev/null
